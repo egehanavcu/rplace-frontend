28d457c7dfd309a5c008e1343ecf5c40fe608d72
Egehan <egehanavcu@gmail.com>
2024-01-30 17:19:44 +0300

    Added white and black colors, fixed black color toString bug

--- src/app/_components/colorBox.js
+++ src/app/_components/colorBox.js
@@ -1,27 +1,24 @@
+const toHexWithLeadingZeros = (number) => {
+  let hexString = number.toString(16);
+  while (hexString.length < 6) {
+    hexString = "0" + hexString;
+  }
+  return hexString;
+};
+
 export default function ColorBox({
   index,
   color,
   hoveredColor,
   setHoveredColor,
 }) {
-  const hexToRgb = (hex) => {
-    var bigint = parseInt(hex, 16);
-    var r = (bigint >> 16) & 255;
-    var g = (bigint >> 8) & 255;
-    var b = bigint & 255;
-
-    return r + "," + g + "," + b;
-  };
-
   return (
     <div
       id={`color-${index}`}
-      className="flex-1 w-16 h-8 transition-colors cursor-pointer border"
+      className="flex-1 w-16 h-8 transition-opacity cursor-pointer border"
       style={{
-        backgroundColor:
-          color === hoveredColor
-            ? `rgba(${hexToRgb(color.toString(16))}, 0.3)`
-            : `#${color.toString(16)}`,
+        backgroundColor: `#${toHexWithLeadingZeros(color)}`,
+        opacity: color === hoveredColor ? 0.3 : 1,
       }}
       onMouseEnter={() => {
         setHoveredColor(color);
--- src/app/_utils/constants.js
+++ src/app/_utils/constants.js
@@ -1,7 +1,8 @@
 export const PixelSize = 15;
-export const CanvasWidth = 150 * PixelSize;
-export const CanvasHeight = 150 * PixelSize;
+export const CanvasWidth = 500 * PixelSize;
+export const CanvasHeight = 500 * PixelSize;
 export const PlacementSeconds = 3;
 export const COLORS = [
-  0xfca5a5, 0xfde047, 0x86efac, 0x93c5fd, 0xa5b4fc, 0xd8b4fe, 0xf9a8d4,
+  0x000000, 0xffffff, 0xfca5a5, 0xfde047, 0x86efac, 0x93c5fd, 0xa5b4fc,
+  0xd8b4fe, 0xf9a8d4,
 ];
