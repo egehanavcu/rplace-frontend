31cbdde486e792eee8b1689248555f89073a2b3b
Egehan <egehanavcu@gmail.com>
2024-02-16 19:21:21 +0300

    Better loading UI

--- src/app/play/page.js
+++ src/app/play/page.js
@@ -1,14 +1,12 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-//import { useRouter } from "next/router";
+
 const { useEffect } = require("react");
 
 export default function AuthenticationPage() {
-  //const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
-  console.log("TOKEN BU ", token);
 
   if (token) {
     useEffect(() => {
@@ -18,12 +16,18 @@ export default function AuthenticationPage() {
       )
         .then((response) => response.json())
         .then((receivedData) => {
-          console.log("oluşturuldu");
-          //router.push("/");
+          window.location.href = "/";
         })
         .catch((error) => console.error("Parsing error: ", error));
     }, []);
 
-    return <b className="text-white">Yönlendirilirken lütfen bekleyiniz.</b>;
+    return (
+      <div className="flex flex-col justify-center items-center gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <img src="/images/loading.gif" className="w-28 h-28" />
+        <b className="text-2xl text-white text-center select-none">
+          Yönlendirilirken lütfen bekleyiniz...
+        </b>
+      </div>
+    );
   }
 }
