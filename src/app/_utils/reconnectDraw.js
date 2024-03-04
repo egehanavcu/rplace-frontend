df66babb14da3080a259d68c6fc80187ef951dde
Egehan <egehanavcu@gmail.com>
2024-03-04 23:21:37 +0300

    Fixed major bug because of batchDraw method

--- src/app/_utils/reconnectDraw.js
+++ src/app/_utils/reconnectDraw.js
@@ -3,6 +3,9 @@ import { PixelSize } from "./constants";
 
 export const reconnectDraw = function (data) {
   getBoardReconnect(this.lastHeartbeatAt).then((data) => {
+    /*
+    // ESKİ YÖNTEM
+    // Bazen tuval yeni piksel ekleyene kadar gözükmüyordu, bu yüzden yeniden bağlanmada batchDraw metodunu kullanmayı bıraktım.
     this.renderTexture.beginDraw();
     for (const dataPixel of data) {
       this.renderTexture.batchDraw(
@@ -12,5 +15,21 @@ export const reconnectDraw = function (data) {
       );
     }
     this.renderTexture.endDraw();
+    */
+
+    for (const dataPixel of data) {
+      this.renderTexture.draw(
+        new Phaser.GameObjects.Rectangle(
+          this,
+          0,
+          0,
+          PixelSize,
+          PixelSize,
+          parseInt(dataPixel.color, 16)
+        ),
+        dataPixel.x * PixelSize + PixelSize / 2,
+        dataPixel.y * PixelSize + PixelSize / 2
+      );
+    }
   });
 };
