05aed2c755869b492acc13c861f9aa41f8aa7c57
Egehan <egehanavcu@gmail.com>
2024-03-04 23:22:32 +0300

    Deleted unneccessary comment blocks

--- src/app/_utils/reconnectDraw.js
+++ src/app/_utils/reconnectDraw.js
@@ -3,20 +3,6 @@ import { PixelSize } from "./constants";
 
 export const reconnectDraw = function (data) {
   getBoardReconnect(this.lastHeartbeatAt).then((data) => {
-    /*
-    // ESKİ YÖNTEM
-    // Bazen tuval yeni piksel ekleyene kadar gözükmüyordu, bu yüzden yeniden bağlanmada batchDraw metodunu kullanmayı bıraktım.
-    this.renderTexture.beginDraw();
-    for (const dataPixel of data) {
-      this.renderTexture.batchDraw(
-        this.batchPixels[parseInt(dataPixel.color, 16)],
-        dataPixel.x * PixelSize + PixelSize / 2,
-        dataPixel.y * PixelSize + PixelSize / 2
-      );
-    }
-    this.renderTexture.endDraw();
-    */
-
     for (const dataPixel of data) {
       this.renderTexture.draw(
         new Phaser.GameObjects.Rectangle(
