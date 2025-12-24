400bf52eb39112dd44da352ae27db36d5f1e129c
Egehan <egehanavcu@gmail.com>
2025-12-24 12:13:23 +0300

    Added obfuscation to final build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,50 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const WebpackObfuscator = require("webpack-obfuscator");
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.module.rules.push({
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, /\.next/],
+        enforce: "post",
+        use: [
+          {
+            loader: WebpackObfuscator.loader,
+            options: {
+              rotateStringArray: true,
+              stringArray: true,
+              stringArrayThreshold: 0.75,
+              compact: true,
+              identifierNamesGenerator: "hexadecimal",
+              disableConsoleOutput: true,
+              controlFlowFlattening: true,
+              controlFlowFlatteningThreshold: 0.5,
+              splitStrings: false,
+              debugProtection: false,
+
+              ignoreImports: true,
+
+              reservedStrings: [
+                "use client",
+                "use server",
+                "react",
+                "react-dom",
+                "phaser",
+                "Phaser",
+              ],
+            },
+          },
+        ],
+      });
+    }
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
